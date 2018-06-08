f5852ebcf581cbdfaab943aa241ed09dfa72d206
Mohamed Sami Mejri <medsamimejri@gmail.com>
2018-06-08 13:59:39 +0100

    work with drawer

--- lib/second_screen.dart
+++ lib/second_screen.dart
@@ -1,10 +1,10 @@
 import 'package:econstat/all_users.dart';
 import 'package:econstat/profile.dart';
 import 'package:flutter/material.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class SecondScreen extends StatelessWidget {
-  var idUser;
-  SecondScreen({Key key, this.idUser}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
@@ -13,14 +13,23 @@ class SecondScreen extends StatelessWidget {
         primarySwatch: Colors.orange,
         primaryColor: Colors.deepOrange,
       ),
-      home: SecondPage(idUser: this.idUser),
+      home: SecondPage(),
     );
   }
 }
 
 class SecondPage extends StatelessWidget {
-  var idUser;
-  SecondPage({Key key, this.idUser}) : super(key: key);
+  var idUser,username,firstname,lastname;
+  SecondPage({Key key, this.idUser,this.firstname,this.lastname,this.username}) : super(key: key);
+
+  _launchURL() async {
+    const url = 'tel:27181132';
+    if (await canLaunch(url)) {
+      await launch(url);
+    } else {
+      throw 'Could not make Call';
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -32,37 +41,36 @@ class SecondPage extends StatelessWidget {
           child: new ListView(
         children: <Widget>[
           new DrawerHeader(
-            child: new Center(
-              child: new Column(
-                children: <Widget>[
-                  new Expanded(
-                    child: Row(
-                      children: <Widget>[
-                        new Center(
-                          child: new Container(
-                            width: 70.0,
-                            height: 70.0,
-                            decoration: new BoxDecoration(
-                              shape: BoxShape.circle,
-                              image: new DecorationImage(
-                                  fit: BoxFit.fill,
-                                  image: new NetworkImage(
-                                      "https://i.imgur.com/BoN9kdC.png")),
-                            ),
-                          ),
+            child: new Row(
+              children: <Widget>[
+                new Expanded(
+                  child: Row(
+                    children: <Widget>[
+                      new Container(
+                        width: 70.0,
+                        height: 70.0,
+                        decoration: new BoxDecoration(
+                          shape: BoxShape.circle,
+                          image: new DecorationImage(
+                              fit: BoxFit.fill,
+                              image: new NetworkImage(
+                                  "https://i.imgur.com/BoN9kdC.png")),
                         ),
-                        new Column(
+                      ),
+                      new Padding(
+                        padding: const EdgeInsets.all(24.0),
+                        child: new Column(
                           children: <Widget>[
-                            Text("Header"),
-                            Text("Header"),
-                            Text("Header"),
+                            Text("User Name : ${this.username}"),
+                            Text("First Name : ${this.firstname}"),
+                            Text("Last Name : ${this.lastname}"),
                           ],
                         ),
-                      ],
-                    ),
-                  )
-                ],
-              ),
+                      ),
+                    ],
+                  ),
+                )
+              ],
             ),
           ),
           new ListTile(
@@ -75,7 +83,9 @@ class SecondPage extends StatelessWidget {
           ),
           new ListTile(
             title: new Text('Call Customer Service'),
-            onTap: () {},
+            onTap: () {
+              _launchURL();
+            },
           ),
           new Divider(),
           new ListTile(
@@ -115,7 +125,9 @@ class SecondPage extends StatelessWidget {
               MaterialButton(
                 minWidth: 170.0,
                 color: Theme.of(context).accentColor,
-                onPressed: () {},
+                onPressed: () {
+                  _launchURL();
+                },
                 child: Text("Call Customer Service"),
               ),
               MaterialButton(
